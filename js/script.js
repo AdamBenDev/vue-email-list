1685ceb01fd61ffbf782db628b65fcaca638e83f
AdamBenDev <adamaya9732@gmail.com>
2023-04-12 05:18:15 +0200

    errore message,email numbers,visually hidden,email

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 console.log('JS OK');
 console.log('Vue OK', Vue);
+
+
+const app = Vue.createApp({
+    name: 'Email List API',
+    data(){
+        return {
+            emails: [],
+            apiEmail: 'https://flynn.boolean.careers/exercises/api/random/mail',
+            errorMessage: '',
+            emailNumbers: 10
+        }
+    }, 
+});
